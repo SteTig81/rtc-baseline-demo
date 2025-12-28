8622321f27ce23f539c35fe68db16ba6f43c9805
Stefan <stefan-tiger@gmx.de>
2025-12-28 14:48:46 +0100

    impoved bat

--- run_demo.bat
+++ run_demo.bat
@@ -1,8 +1,19 @@
 
 @echo off
-py -3.14 -m venv venv
+if not exist venv (
+    where py >nul 2>nul
+    if %ERRORLEVEL% equ 0 (
+        py -3.14 -m venv venv
+    ) else (
+        echo ERROR: py.exe is NOT found in the system PATH
+        echo HINT: consider to install the Python install manager from https://www.python.org/downloads/
+        goto END    
+    )
+)
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 python main.py --component MyComponent
-python -m http.server 8080
+start python -m http.server 8085
+start http://[::1]:8085/
+:END
 pause
